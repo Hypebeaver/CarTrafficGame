4d9a51f5238bfc583789aeb5e46eef1ba6f94e6f
Johnlooi <106057661+Hypebeaver@users.noreply.github.com>
2023-12-01 20:21:28 +0800

    Create car.h

---
+++ car.h
@@ -0,0 +1,9 @@
+#ifndef CAR_H
+#define CAR_H
+
+int checkRandomNum(PlayerAndCar*, char**, int);
+char randomDirection(PlayerAndCar*, char**);
+void updateCarDirection(PlayerAndCar*, char**);
+int checkAccident(PlayerAndCar*);
+
+#endif
